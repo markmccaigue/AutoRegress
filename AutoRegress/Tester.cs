52966788fd0f1897e6a967df7c0b6376611b438a
markmni <mark@squeakstudios.com>
2014-06-05 16:47:37 +0100

    Initial Code
    Adding AutoRegress and tests

---
+++ AutoRegress/Tester.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AutoRegress
+{
+    public class Tester
+    {
+        private static string GetFilePathForObject<T>(T target)
+        {
+            return string.Format("{0}{1}{2}{3}{4}{5}", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Path.DirectorySeparatorChar, "AutoRegress", Path.DirectorySeparatorChar, target.GetType().Name, ".json");
+        }
+
+        private static string GetResultsForObject<T>(T target)
+        {
+            var type = target.GetType();
+
+            var methodsAndAccessors = type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+
+            //!IsSpecialName excludes property accessors
+            var methodsWithoutParamaters = methodsAndAccessors.Where(m => m.GetParameters().Length == 0 && !m.IsSpecialName).OrderBy(m => m.Name);
+
+            var methodsAndResults = new Dictionary<string, object>();
+            methodsWithoutParamaters.ToList().ForEach(m =>
+            {
+                var result = m.Invoke(target, null);
+                methodsAndResults.Add(m.Name, result);
+            });
+
+            var serialisedResults = JsonConvert.SerializeObject(methodsAndResults);
+            return serialisedResults;
+        }
+
+        public static void StoreStateForClass<T>(T target)
+        {
+            var serialisedResults = GetResultsForObject(target);
+            File.WriteAllText(GetFilePathForObject(target), serialisedResults);
+        }
+
+        public static bool CheckStateForClass<T>(T target)
+        {
+            var observed = GetResultsForObject(target);
+            var actual = File.ReadAllText(GetFilePathForObject(target));
+            return observed.Equals(actual);
+        }
+    }
+}
---
+++ AutoRegressTests/TesterTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoRegress;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AutoRegressTests
+{
+    [TestClass]
+    public class TesterTests
+    {
+        private string _path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + Path.DirectorySeparatorChar + "AutoRegress";
+
+        [TestInitialize()]
+        public void Startup()
+        {
+            System.IO.Directory.CreateDirectory(_path);
+            var directory = new System.IO.DirectoryInfo(_path);
+            directory.EnumerateFiles().ToList().ForEach(f => f.Delete());
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            System.IO.Directory.Delete(_path, recursive: true);
+        }
+
+        private class ClassUnderTest
+        {
+            public bool ShouldFail { get; set; }
+
+            public int GetNumber()
+            {
+                return ShouldFail ? 2 : 1;
+            }
+
+            public static int GetNumberStatic()
+            {
+                return 1;
+            }
+
+            public int Echo(int number)
+            {
+                return number;
+            }
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TestThrowsExceptionForUnknownClass()
+        {
+            Tester.CheckStateForClass(new ClassUnderTest());
+        }
+
+        [TestMethod]
+        public void TestCompareWorksForCorrectCase()
+        {
+            var objectUnderTest = new ClassUnderTest();
+            Tester.StoreStateForClass(objectUnderTest);
+            var result = Tester.CheckStateForClass(objectUnderTest);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestCompareWorksForIncorrectCase()
+        {
+            var objectUnderTest = new ClassUnderTest();
+            Tester.StoreStateForClass(objectUnderTest);
+            objectUnderTest.ShouldFail = true;
+            var result = Tester.CheckStateForClass(objectUnderTest);
+            Assert.IsFalse(result);
+        }
+    }
+}
